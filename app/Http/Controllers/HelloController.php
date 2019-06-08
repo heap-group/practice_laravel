d52dcae73f968b85635e626fd7e3e8f14417c2fc
ysk <y.sawa4787@gmail.com>
2019-06-08 19:34:08 +0900

    edit 独自バリテーター　エラーメッセージの日本語化

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,27 +10,38 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
+//        $validator = Validator::make($request->all(), [
+//            'id' => 'required',
+//            'pass' => 'required',
+//        ]);
+//
+//        if ($validator->fails()) {
+//            $msg = 'クエリーに問題があります';
+//        } else {
+//            $msg = '正常に受け付けました';
+//        }
 
-        if ($validator->fails()) {
-            $msg = 'クエリーに問題があります';
-        } else {
-            $msg = '正常に受け付けました';
-        }
+        $msg = 'フォームを入力してください';
 
         return view('hello.index', ['msg' => $msg]);
     }
 
     public function post(Request $request)
     {
-        $validator = Validator::make($request->all(), [
+        $rules = [
             'name' => 'required',
             'mail' => 'email',
             'age' => 'numeric|between:0,150',
-        ]);
+        ];
+
+        $errMsgs = [
+          'name.required' => '名前は必須です',
+          'mail.email' => 'メールアドレスは必須です',
+          'age.numeric' => '年齢は数値でしか入力できません',
+          'age.between' => '年齢は０〜１５０までしか入力できません',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $errMsgs);
 
         if ($validator->fails()) {
             return redirect('/hello')
