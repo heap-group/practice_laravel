5afefffd047620968dad7f08904e3ed2d5b603ed
ysk <y.sawa4787@gmail.com>
2019-06-08 19:20:48 +0900

    edit 独自バリテーターの作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\HelloRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class HelloController extends Controller
 {
@@ -12,8 +13,20 @@ class HelloController extends Controller
         return view('hello.index', ['msg' => 'フォームを入力してください']);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
-        return view('hello.index', ['msg' => '正しく入力されました']);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/hello')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        return view('hello.index', ['msg' => '正しく入力してください']);
     }
 }
