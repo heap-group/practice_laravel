ce300f8c270519f1d98abc27cbf8280f3b74a44f
ysk <y.sawa4787@gmail.com>
2019-06-08 21:09:10 +0900

    edit ちょっとしたカスタマイズバリテータを使いたい時

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
-use Illuminate\Validation\Validator;
+//use Illuminate\Validation\Validator;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Validators\HelloValidator;
 
 class HelloServiceProvider extends ServiceProvider
@@ -26,9 +27,9 @@ class HelloServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $validator = $this->app['validator'];
-        $validator->resolver(function ($translator, $data, $rules, $message){
-            return new HelloValidator($translator, $data, $rules, $message);
+        Validator::extend('hello', function ($attribute, $value, $parameters, $validator){
+            return $value % 2 == 0;
         });
+
     }
 }
