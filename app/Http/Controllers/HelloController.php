7164dbe202411eb74ae2e112c4a5fd6b38e59b2f
ysk <y.sawa4787@gmail.com>
2019-06-08 10:41:27 +0900

    edit Bladeテンプレートの表示

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    public function index(Request $request)
+    {
+        $data = [
+            'msg' => 'Bladeを利用したサンプル',
+        ];
+        return view('hello.index', $data);
+    }
+}
---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Hello index</title>
+    </head>
+    <body>
+        <h1>Bladeの利用</h1>
+        <p>{{ $msg }}</p>
+    </body>
+</html>
