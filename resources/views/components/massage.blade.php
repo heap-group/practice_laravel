4ed8e183d67297ced8734117cf33bffdc81cae27
ysk <y.sawa4787@gmail.com>
2019-06-08 12:12:57 +0900

    edit @slotの使い方

---
+++ resources/views/components/massage.blade.php
@@ -0,0 +1,4 @@
+<div class="">
+    <p>{{ $message_title }}</p>
+    <p>{{ $message_content }}</p>
+</div>
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,6 +10,16 @@
 @section('content')
     <p>本文コンテンツ</p>
     <p>本文コンテンツ</p>
+
+    @component('components.massage')
+        @slot('message_title')
+            CAUTION!
+        @endslot
+
+        @slot('message_content')
+            これはメッセージの表示です
+        @endslot
+    @endcomponent
 @endsection
 
 @section('footer')
