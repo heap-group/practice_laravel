0922839b8219bae24b026eaa78a1b009f7696886
ysk <y.sawa4787@gmail.com>
2019-06-08 22:25:07 +0900

    edit DBクラス（クエリビルダー）プレースホルダーを使ったWHERE検索

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,7 +10,12 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::select('SELECT * FROM people');
+        if (isset($request->id)) {
+            $param = ['id' => $request->id];
+            $items = DB::select('SELECT * FROM people WHERE id = :id', $param);
+        } else {
+            $items = DB::select('SELECT * FROM people');
+        }
 
         return view('hello.index', ['items' => $items]);
     }
