664179caf99d5031ec44d982463d64e3dd81b7f9
ysk <y.sawa4787@gmail.com>
2019-06-08 21:30:15 +0900

    edit Cookieの読み書き

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,60 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\HelloRequest;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-//        $validator = Validator::make($request->all(), [
-//            'id' => 'required',
-//            'pass' => 'required',
-//        ]);
-//
-//        if ($validator->fails()) {
-//            $msg = 'クエリーに問題があります';
-//        } else {
-//            $msg = '正常に受け付けました';
-//        }
-
-        $msg = 'フォームを入力してください';
+        if ($request->hasCookie('msg')) {
+            $msg = 'Cookie '. $request->cookie('msg');
+        } else {
+            $msg = 'cookieはありません';
+        }
 
         return view('hello.index', ['msg' => $msg]);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
-//        $rules = [
-//            'name' => 'required',
-//            'mail' => 'email',
-//            'age' => 'numeric',
-//        ];
-//
-//        $errMsgs = [
-//          'name.required' => '名前は必須です',
-//          'mail.email' => 'メールアドレスは必須です',
-//          'age.numeric' => '年齢は数値でしか入力できません',
-//          'age.min' => '年齢は０以上',
-//          'age.max' => '年齢は２００以下',
-//        ];
-//
-//        $validator = Validator::make($request->all(), $rules, $errMsgs);
-//
-//        $validator->sometimes('age', 'min:0', function ($input){
-//            return !is_int($input->age);
-//        });
-//
-//        $validator->sometimes('age', 'max:200', function ($input){
-//            return !is_int($input->age);
-//        });
-//
-//        if ($validator->fails()) {
-//            return redirect('/hello')
-//                ->withErrors($validator)
-//                ->withInput();
-//        }
+        $rules = [
+            'msg' => 'required',
+        ];
 
-        return view('hello.index', ['msg' => '正しく入力されました！']);
+        $this->validate($request, $rules);
+        $msg = $request->msg;
+        $response = new Response(view('hello.index', ['msg' => 'cookie '. $msg]));
+        $response->cookie('msg', $msg, 100);
+        return $response;
     }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -14,36 +14,46 @@
     @endif
     <table>
         <form action="/hello" method="post">
-{{--            {{ csrf_field() }}--}}
-            @if($errors->has('name'))
-            <tr>
-                <th>エラー：</th>
-                <td>{{ $errors->first('name') }}</td>
-            </tr>
-            @endif
-            <tr>
-                <th>name:</th>
-                <td><input type="text" name="name" value="{{ old('name') }}"></td>
-            </tr>
-            @if($errors->has('mail'))
+            {{ csrf_field() }}
+{{--            @if($errors->has('name'))--}}
+{{--            <tr>--}}
+{{--                <th>エラー：</th>--}}
+{{--                <td>{{ $errors->first('name') }}</td>--}}
+{{--            </tr>--}}
+{{--            @endif--}}
+{{--            <tr>--}}
+{{--                <th>name:</th>--}}
+{{--                <td><input type="text" name="name" value="{{ old('name') }}"></td>--}}
+{{--            </tr>--}}
+{{--            @if($errors->has('mail'))--}}
+{{--                <tr>--}}
+{{--                    <th>エラー：</th>--}}
+{{--                    <td>{{ $errors->first('mail') }}</td>--}}
+{{--                </tr>--}}
+{{--            @endif--}}
+{{--            <tr>--}}
+{{--                <th>mail:</th>--}}
+{{--                <td><input type="email" name="mail" value="{{ old('mail') }}"></td>--}}
+{{--            </tr>--}}
+{{--            @if($errors->has('age'))--}}
+{{--                <tr>--}}
+{{--                    <th>エラー：</th>--}}
+{{--                    <td>{{ $errors->first('age') }}</td>--}}
+{{--                </tr>--}}
+{{--            @endif--}}
+{{--            <tr>--}}
+{{--                <th>age:</th>--}}
+{{--                <td><input type="text" name="age" value="{{ old('age') }}"></td>--}}
+{{--            </tr>--}}
+            @if($errors->has('msg'))
                 <tr>
                     <th>エラー：</th>
-                    <td>{{ $errors->first('mail') }}</td>
+                    <td>{{ $errors->first('msg') }}</td>
                 </tr>
             @endif
             <tr>
-                <th>mail:</th>
-                <td><input type="email" name="mail" value="{{ old('mail') }}"></td>
-            </tr>
-            @if($errors->has('age'))
-                <tr>
-                    <th>エラー：</th>
-                    <td>{{ $errors->first('age') }}</td>
-                </tr>
-            @endif
-            <tr>
-                <th>age:</th>
-                <td><input type="text" name="age" value="{{ old('age') }}"></td>
+                <th>msg:</th>
+                <td><input type="text" name="msg" value="{{ old('msg') }}"></td>
             </tr>
             <tr>
                 <td><input type="submit" value="submit"></td>
